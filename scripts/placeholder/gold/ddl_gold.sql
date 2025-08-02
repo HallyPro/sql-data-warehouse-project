abaa2a385c5e6b0f33074e21ec6fc91cb7cd06c5
HallyPro <108992335+HallyPro@users.noreply.github.com>
2025-08-02 19:37:24 +0200

    Update ddl_gold.sql

--- scripts/placeholder/gold/ddl_gold.sql
+++ scripts/placeholder/gold/ddl_gold.sql
@@ -1,7 +1,22 @@
+/*
+======================================================================================
+DDL Scripts: Create Gold Views
+======================================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse.
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+======================================================================================
+*/
+
 -- ======================================================================================
 -- Create Dimension Table: gold.dim_customers
 -- ======================================================================================
-
 IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
     DROP VIEW gold.dim_customers;
 GO
@@ -30,7 +45,6 @@ GO
 -- ======================================================================================
 -- Create Dimension Table: gold.dim_products
 -- ======================================================================================
-
 IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
     DROP VIEW gold.dim_products;
 GO
@@ -57,7 +71,6 @@ GO
 -- ======================================================================================
 -- Create Fact Table: gold.fact_sales
 -- ======================================================================================
-
 IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
     DROP VIEW gold.fact_sales;
 GO
